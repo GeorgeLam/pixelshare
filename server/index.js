8f02ed8590c1cf56e6794aeb229244e173a1f62c
George Lam <2940746+GeorgeLam@users.noreply.github.com>
2020-10-02 21:03:17 +0100

    Adjust for heroku3

--- server/index.js
+++ server/index.js
@@ -353,4 +353,5 @@ app.post("/updateProfile", function (req, res) {
   });
 });
 
-app.listen(5000, () => console.log("Server started on port 5000"));
+let port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Server started on port ${port}`));
