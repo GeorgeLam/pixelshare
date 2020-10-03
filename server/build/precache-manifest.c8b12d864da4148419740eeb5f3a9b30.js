2901596eef2bcdcced2551cfb1c16d5d24ebe798
George Lam <2940746+GeorgeLam@users.noreply.github.com>
2020-10-03 15:08:00 +0100

    Adjust for heroku5

---
+++ server/build/precache-manifest.c8b12d864da4148419740eeb5f3a9b30.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3511dce118fa701e6c72f591cce886d1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "07d595d1f61127901d9a",
+    "url": "/static/css/2.1bce3852.chunk.css"
+  },
+  {
+    "revision": "97a54d7c4edf03df70d4",
+    "url": "/static/css/main.d62b774a.chunk.css"
+  },
+  {
+    "revision": "07d595d1f61127901d9a",
+    "url": "/static/js/2.0d6fb90d.chunk.js"
+  },
+  {
+    "revision": "7bfaa32b315fe466d84ab2ae49052d17",
+    "url": "/static/js/2.0d6fb90d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97a54d7c4edf03df70d4",
+    "url": "/static/js/main.ba51bc2e.chunk.js"
+  },
+  {
+    "revision": "34c6c7d429026c0d1562",
+    "url": "/static/js/runtime-main.a443f87e.js"
+  },
+  {
+    "revision": "a282b009e3024d1ea55513568ee67c81",
+    "url": "/static/media/1.a282b009.jpg"
+  },
+  {
+    "revision": "740028d44a1c1c3d31566dd7e2702d56",
+    "url": "/static/media/trash.740028d4.svg"
+  }
+]);
